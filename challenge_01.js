b32ce1ac2efe0dfcf5b0af5b225e1f35c3c872da
Fran-TP <ftorresp@unasam.edu.pe>
2024-09-30 17:25:22 -0500

    Add function to count sheep with specific color and name pattern

---
+++ challenge_01.js
@@ -0,0 +1,19 @@
+//cSpell: disable
+
+const colorAccepted = 'rojo'
+const regExp = /(?=.*n)(?=.*a)/i
+
+const countSheep = (sheep) => {
+  return sheep.filter(s => s.color === colorAccepted && regExp.test(s.name))
+}
+
+const sheep = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+console.log(countSheep(sheep))
