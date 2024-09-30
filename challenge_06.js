6c5aaa6e6ed836d550c64c028b382b33ab2419b9
Fran-TP <ftorresp@unasam.edu.pe>
2024-09-30 17:33:39 -0500

    Refactor sumPairs function to improve efficiency

--- challenge_06.js
+++ challenge_06.js
@@ -1,13 +1,9 @@
 export default function sumPairs(numbers, result) {
-  const diffs = numbers.map((value, idx) => ({
-    value,
-    diff: result - value,
-    idx,
-  }))
+  for (let i = 0; i < numbers.length; i++) {
+    const target = result - numbers[i]
 
-  for (const { value, diff, idx } of diffs) {
-    if (numbers.includes(diff) && numbers.lastIndexOf(diff) > idx) {
-      return [value, diff]
+    if (numbers.includes(target) && numbers.lastIndexOf(target) > i) {
+      return [numbers[i], target]
     }
   }
 
