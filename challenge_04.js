feb44b92bcb9b62585cb059e5a6f63df9e5026ae
Fran-TP <ftorresp@unasam.edu.pe>
2024-09-30 23:08:58 -0500

    Refactor createXmasTree function to improve code readability and efficiency

---
+++ challenge_04.js
@@ -0,0 +1,11 @@
+export default function createXmasTree(height) {
+  let tree = ''
+  let trunk = `${'_'.repeat(height - 1)}#${'_'.repeat(height - 1)}`
+  for (let i = 1; i <= height; i++) {
+    tree += `${'_'.repeat(height - (i - 1) - 1)}${'*'.repeat(
+      2 * i - 1
+    )}${'_'.repeat(height - (i - 1) - 1)}${i === height ? '' : '\n'}`
+  }
+
+  return `${tree}\n${trunk}\n${trunk}\n`.trim()
+}
