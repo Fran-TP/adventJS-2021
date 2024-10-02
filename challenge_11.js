6cbe08402727394b4b2678fe130d810f17dc606d
Fran-TP <ftorresp@unasam.edu.pe>
2024-10-02 17:10:49 -0500

    Refactor shouldBuyFidelity function for improved readability and efficiency

--- challenge_11.js
+++ challenge_11.js
@@ -1,4 +1,17 @@
 // implicit solution
-export default function shouldBuyFidelity(times) {
+export default function fShouldBuyFidelity(times) {
   return times > 23
+}
+
+export const shouldBuyFidelity = times => {
+  const SINGLE_TICKET_COST = 12
+  const FIDELITY_CARD_COST = 250
+
+  const seriesSum =
+    (SINGLE_TICKET_COST * (1 - Math.pow(0.75, times))) / (1 - 0.75)
+
+  const totalSingleTicketCost = SINGLE_TICKET_COST * times
+  const totalFidelityCardCost = FIDELITY_CARD_COST + seriesSum
+
+  return totalFidelityCardCost < totalSingleTicketCost
 }
