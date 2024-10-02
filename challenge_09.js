fe181c088c1b7fc6f26396cbdfa6eac50dd7a6ed
Fran-TP <ftorresp@unasam.edu.pe>
2024-10-02 15:19:17 -0500

    Refactor groupBy function to improve code readability and efficiency

--- challenge_09.js
+++ challenge_09.js
@@ -1,19 +1,38 @@
 const groupBy = (collection, it) => {
   let object = {}
+  const cb = typeof it === 'function' ? it : v => v[it]
 
   for (const item of collection) {
-    if (typeof it === 'function') {
-      object = {
-        ...object,
-        [it(item)]: collection.filter(v => it(v) === it(item)),
-      }
-    } else {
-      object = {
-        ...object,
-        [item[it]]: collection.filter(v => v[it] === item[it]),
-      }
-    }
+    object[cb(item)] ??= []
+    object[cb(item)].push(item)
   }
 
   return object
 }
+
+// Test cases
+
+console.log(groupBy([6.1, 4.2, 6.3], Math.floor)) // { 6: [6.1, 6.3], 4: [4.2] }
+console.log(groupBy(['one', 'two', 'three'], 'length')) // { 3: ['one', 'two'], 5: ['three'] }
+console.log(groupBy([{ age: 23 }, { age: 24 }], 'age')) // { 23: [{age: 23}], 24: [{age: 24}] }
+
+console.log(
+  groupBy([1397639141184, 1363223700000], timestamp =>
+    new Date(timestamp).getFullYear()
+  )
+)
+// { 2013: [1363223700000], 2014: [1397639141184] }
+
+console.log(
+  groupBy(
+    [
+      { title: 'JavaScript: The Good Parts', rating: 8 },
+      { title: 'Aprendiendo Git', rating: 10 },
+      { title: 'Clean Code', rating: 9 },
+    ],
+    'rating'
+  )
+)
+// { 8: [{ title: 'JavaScript: The Good Parts', rating: 8 }],
+//   9: [{ title: 'Clean Code', rating: 9 }],
+//   10: [{ title: 'Aprendiendo Git', rating: 10 }] }
