3627203c197f412fd556289013bafc326c734925
Fran-TP <ftorresp@unasam.edu.pe>
2024-10-09 11:17:37 -0500

    Refactor fixFiles function to use a Map for better performance and readability

--- challenge_18.js
+++ challenge_18.js
@@ -1,30 +1,28 @@
 const fixFiles = files => {
   const filesMap = new Map()
-  const result = []
 
   for (const file of files) {
     if (!filesMap.has(file)) {
       filesMap.set(file, 0)
-      result.push(file)
     } else {
       const count = filesMap.get(file) + 1
       const fileName = `${file}(${count})`
 
       filesMap.set(file, count)
-      result.push(fileName)
+      filesMap.set(fileName, 0)
     }
   }
 
-  return result
+  return [...filesMap.keys()]
 }
 
 // test
 const files = ['photo', 'postcard', 'photo', 'photo', 'video']
 console.log(fixFiles(files)) // ['photo', 'postcard', 'photo(1)', 'photo(2)', 'video']
 
-// const files2 = ['file', 'file', 'file', 'game', 'game']
-// console.log(fixFiles(files2)) // ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
+const files2 = ['file', 'file', 'file', 'game', 'game']
+console.log(fixFiles(files2)) // ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
 
-// // ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
-// const files3 = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
-// console.log(fixFiles(files3)) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
+// ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
+const files3 = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
+console.log(fixFiles(files3)) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
