852cd354a53a9aa98a532eec45f8d197eb97c44c
Fran-TP <ftorresp@unasam.edu.pe>
2024-10-12 18:39:03 -0500

    Add documentation and optimize canReconfigure function for better performance

--- challenge_23.js
+++ challenge_23.js
@@ -1,23 +1,29 @@
+/**
+ * Checks if one string can be reconfigured to another string.
+ *
+ * @param {string} from - The original string.
+ * @param {string} to - The target string.
+ * @returns {boolean} True if the original string can be reconfigured to the target string, false otherwise.
+ */
 const canReconfigure = (from, to) => {
   if (from.length !== to.length) return false
 
-  const mapFromTo = {}
-  const mapToFrom = {}
+  const map = new Map()
 
   for (let i = 0; i < from.length; i++) {
     const charFrom = from[i]
     const charTo = to[i]
 
-    if (mapFromTo[charFrom] && mapFromTo[charFrom] !== charTo) {
+    if (map.has(charFrom) && map.get(charFrom) !== charTo) {
       return false
     }
 
-    if (mapToFrom[charTo] && mapToFrom[charTo] !== charFrom) {
+    if (map.has(charTo) && map.get(charTo) !== charFrom) {
       return false
     }
 
-    mapFromTo[charFrom] = charTo
-    mapToFrom[charTo] = charFrom
+    map.set(charFrom, charTo)
+    map.set(charTo, charFrom)
   }
 
   return true
@@ -27,4 +33,4 @@ const canReconfigure = (from, to) => {
 console.log(canReconfigure('BAL', 'LIB')) // true
 console.log(canReconfigure('CON', 'JON')) // true
 console.log(canReconfigure('XBOX', 'PSPS')) // false
-console.log(canReconfigure('AA', 'CD')) // false
+console.log(canReconfigure('AA', 'CD')) // true
